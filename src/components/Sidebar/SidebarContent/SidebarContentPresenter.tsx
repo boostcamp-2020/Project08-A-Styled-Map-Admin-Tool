e8a5dd0450c4ba56e3ef926af90a6fe3c6e97cd9
eunsol0410 <eunsollee04@gmail.com>
2020-11-18 18:47:29 +0900

    [Feat] [#1] 좌측 기본 사이드바 컨텐트 레이아웃
    - 아주 기본적인 구조만 설정함

---
+++ src/components/Sidebar/SidebarContent/SidebarContentPresenter.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from '../../../utils/styles/styled';
+
+function SidebarContentPresenter(): React.ReactElement {
+  return <div />;
+}
+
+export default SidebarContentPresenter;
