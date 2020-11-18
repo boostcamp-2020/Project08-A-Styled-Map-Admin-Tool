62684fed988b72c34a035dc87e2e599fd82e8816
eunsol0410 <eunsollee04@gmail.com>
2020-11-18 18:44:33 +0900

    [feat] [#6] 사이드바 헤더
    - 기본적인 레이아웃 구현
    - Button Icon SVG로 구현
    - Default Props 관련 에러 수정을 위해 린트 수정

---
+++ src/components/Sidebar/SidebarHeader/SidebarHeaderContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SidebarHeaderPresenter from './SidebarHeaderPresenter';
+
+function SidebarHeaderContainer(): React.ReactElement {
+  return <SidebarHeaderPresenter />;
+}
+
+export default SidebarHeaderContainer;
