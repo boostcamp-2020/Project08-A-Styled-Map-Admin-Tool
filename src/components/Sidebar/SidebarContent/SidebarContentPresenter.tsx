bea3b10fe1e56fd9cf37b04e547406973f030660
박은식 <qkrdmstlr3@naver.com>
2020-11-19 11:59:20 +0900

    [feat] [#1] 기본설정 사이드바의 테마설정 UI구현
    - 기본설정 사이드바에서 테마설정 UI부분을 구현하였다.
    - input radio를 쓰지 않고 state를 통해서 선택을 구현

--- src/components/Sidebar/SidebarContent/SidebarContentPresenter.tsx
+++ src/components/Sidebar/SidebarContent/SidebarContentPresenter.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from '../../../utils/styles/styled';
+import SidebarContentThemeContainer from './SidebarContentTheme/SidebarContentThemeContainer';
+
+const ContentWrapper = styled.main`
+  width: 80%;
+  margin: 0 auto;
+`;
 
 function SidebarContentPresenter(): React.ReactElement {
-  return <div />;
+  return (
+    <ContentWrapper>
+      <SidebarContentThemeContainer />
+    </ContentWrapper>
+  );
 }
 
 export default SidebarContentPresenter;
