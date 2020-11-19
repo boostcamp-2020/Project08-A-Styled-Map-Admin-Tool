a4acad4c655f6dc8bd32da4a789551fba65bb0a2
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-19 19:05:42 +0900

    [feat] [#3] 세부설정 사이드바 레이아웃 연결 및 토글
    - 세부설정 사이드바 레이아웃 연결
    - 되돌리기 및 사이드바 토글 구현
    - 선택에 따른 사이드바 너비 변화

--- src/components/Sidebar/SidebarContentMore/SidebarContentContainer.tsx
+++ src/components/Sidebar/SidebarContentMore/SidebarContentContainer.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SidebarContentPresenter from './SidebarContentPresenter';
+import StylerPresenter from './StylerPresenter';
+import FeatureTypePresenter from './FeatureTypePresenter';
+import DetailTypePresenter from './DetailTypePresenter';
 
 function SidebarContentContainer(): React.ReactElement {
-  return <SidebarContentPresenter />;
+  const [featureName, setFeatureName] = useState('');
+  const [detailName, setDetailName] = useState(''); // (구역 채우기) 이런 형태
+  const [styledFeatureList, setStyledFeatureList] = useState([]); // 추후 전역 상태로 변경
+
+  const featureClickHandler = (name: string) => {
+    if (name !== featureName) {
+      setFeatureName(name);
+    }
+  };
+  const detailClickHandler = (name: string) => {
+    if (name !== detailName) {
+      setDetailName(name);
+    }
+  };
+
+  return (
+    <SidebarContentPresenter>
+      <FeatureTypePresenter
+        featureName={featureName}
+        styledFeatureList={styledFeatureList}
+        featureClickHandler={featureClickHandler}
+      />
+      <DetailTypePresenter
+        featureName={featureName}
+        detailName={detailName}
+        detailClickHandler={detailClickHandler}
+      />
+      <StylerPresenter detailName={detailName} />
+    </SidebarContentPresenter>
+  );
 }
 
 export default SidebarContentContainer;
