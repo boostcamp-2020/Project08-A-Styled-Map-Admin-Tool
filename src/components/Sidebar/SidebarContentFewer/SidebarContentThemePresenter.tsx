a8f6295addf9561ae03cf48eb932c085ecc7f9bf
박은식 <qkrdmstlr3@naver.com>
2020-11-20 11:05:48 +0900

    [refactor] [#1] 너무 큰 컴포넌트 쪼개기
    - 너무 크다고 생각되는 컴포넌트를 하위 컴포넌트 몇개로 쪼갬

--- src/components/Sidebar/SidebarContentFewer/SidebarContentThemePresenter.tsx
+++ src/components/Sidebar/SidebarContentFewer/SidebarContentThemePresenter.tsx
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
+import ThemeItemPresenter from './ThemeItemPresenter';
 import styled from '../../../utils/styles/styled';
-
-interface ImageProp {
-  src: string;
-}
-
-interface CheckedProp {
-  checked: boolean;
-}
 
 const ThemeWrapper = styled.div`
   padding: 50px 16px;
@@ -20,45 +13,6 @@ const Title = styled.h2`
 
 const List = styled.ul`
   margin-top: 25px;
-`;
-
-const Item = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 10px 0;
-  cursor: pointer;
-`;
-
-const Image = styled.div<ImageProp>`
-  background-image: url('${(props) => props.src}');
-  background-size: cover;
-  width: 25%;
-  height: 50px;
-`;
-
-const Name = styled.span<CheckedProp>`
-  width: 55%;
-  font-size: 1.5rem;
-  color: ${(props) => (props.checked ? props.theme.GREEN : 'gray')};
-`;
-
-const Checkbox = styled.div<CheckedProp>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 24px;
-  height: 24px;
-  border-radius: 12px;
-  background-color: ${(props) =>
-    props.checked ? props.theme.GREEN : 'lightgray'};
-`;
-
-const Circle = styled.div<CheckedProp>`
-  width: 16px;
-  height: 16px;
-  border-radius: 8px;
-  background-color: ${(props) => (props.checked ? 'white' : 'lightgray')};
 `;
 
 const data = [
@@ -106,13 +60,12 @@ function SidebarContentThemePresenter(): React.ReactElement {
       <Title>지도 테마 선택</Title>
       <List>
         {data.map((d, i) => (
-          <Item key={d.name} onClick={() => clickHandler(i)}>
-            <Image src={d.src} />
-            <Name checked={i === checkedItemIndex}>{d.name}</Name>
-            <Checkbox checked={i === checkedItemIndex}>
-              <Circle checked={i === checkedItemIndex} />
-            </Checkbox>
-          </Item>
+          <ThemeItemPresenter
+            key={d.name}
+            checked={i === checkedItemIndex}
+            clickHandler={() => clickHandler(i)}
+            data={d}
+          />
         ))}
       </List>
     </ThemeWrapper>
--- src/components/Sidebar/SidebarContentMore/StylerPresenter.tsx
+++ src/components/Sidebar/SidebarContentMore/StylerPresenter.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from '../../../utils/styles/styled';
-import { Range } from '../SidebarContentFewer/DepthItemPresenter';
+import ColorStylePresenter from './ColorStylePresenter';
+import WeightStylePresenter from './WeightStylePresenter';
+import SaturationStylePresenter from './SaturationStylePresenter';
+import LightnessStyleWrapper from './LightnessStyleWrapper';
+import VisibilityStylePresenter from './VisibilityStylePresenter';
 
 const StylerWrapper = styled.div`
   height: 100%;
@@ -25,91 +29,6 @@ const Hr = styled.hr`
   margin-bottom: 25px;
 `;
 
-const VisibilityWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const VisibilityTitle = styled.h2`
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: gray;
-  margin-bottom: 10px;
-`;
-
-const VisilityItem = styled.div`
-  font-size: 1.6rem;
-  margin-bottom: 5px;
-`;
-
-const ColorWrapper = styled.div``;
-
-const ColorTitle = styled.label`
-  margin-bottom: 10px;
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: gray;
-`;
-
-const ColorCode = styled.div``;
-
-const ColorPalette = styled.input`
-  margin: 10px 20px;
-  width: 100px;
-  height: 40px;
-`;
-
-const WeightWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 15px 0;
-`;
-
-const WeightTitle = styled.label`
-  margin-bottom: 10px;
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: gray;
-`;
-
-const WeightControlBar = styled(Range)`
-  width: 100%;
-`;
-
-const SaturationWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 15px 0;
-`;
-
-const SaturationTitle = styled.label`
-  margin-bottom: 10px;
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: gray;
-`;
-
-const SaturationControlBar = styled(Range)`
-  width: 100%;
-`;
-
-const LightnessWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 15px 0;
-`;
-
-const LightnessTitle = styled.label`
-  margin-bottom: 10px;
-  font-size: 1.7rem;
-  font-weight: 600;
-  color: gray;
-`;
-
-const LightnessControlBar = styled(Range)`
-  width: 100%;
-`;
-
 interface StylerPresenterProps {
   detailName: string;
 }
@@ -124,49 +43,13 @@ function StylerPresenter({
   return (
     <StylerWrapper>
       <StylerTitle>스타일</StylerTitle>
-      <VisibilityWrapper>
-        <VisibilityTitle>가시성</VisibilityTitle>
-        <VisilityItem>상위요소 상속</VisilityItem>
-        <VisilityItem>보임</VisilityItem>
-        <VisilityItem>숨김</VisilityItem>
-      </VisibilityWrapper>
+      <VisibilityStylePresenter />
       <Hr />
-      <ColorWrapper>
-        <ColorTitle htmlFor="styler__color">색상</ColorTitle>
-        <ColorCode />
-        <ColorPalette type="color" id="styler__color" />
-      </ColorWrapper>
+      <ColorStylePresenter />
       <Hr />
-      <WeightWrapper>
-        <WeightTitle htmlFor="styler__weight">굵기</WeightTitle>
-        <WeightControlBar
-          type="range"
-          min="0"
-          max="8"
-          step="0.5"
-          id="styler__weight"
-        />
-      </WeightWrapper>
-      <SaturationWrapper>
-        <SaturationTitle htmlFor="styler__saturation">채도</SaturationTitle>
-        <SaturationControlBar
-          type="range"
-          min="-100"
-          max="100"
-          step="5"
-          id="styler__saturation"
-        />
-      </SaturationWrapper>
-      <LightnessWrapper>
-        <LightnessTitle htmlFor="styler__lightness">밝기</LightnessTitle>
-        <LightnessControlBar
-          type="range"
-          min="-100"
-          max="100"
-          step="5"
-          id="styler__lightness"
-        />
-      </LightnessWrapper>
+      <WeightStylePresenter />
+      <SaturationStylePresenter />
+      <LightnessStyleWrapper />
     </StylerWrapper>
   );
 }
