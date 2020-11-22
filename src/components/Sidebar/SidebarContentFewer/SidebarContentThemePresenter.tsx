b559d016d2f712010db4b417579297c0d2f4a5b5
박은식 <qkrdmstlr3@naver.com>
2020-11-22 15:27:24 +0900

    [refactor] [#1] 기본설정 사이드바의 간격 조정
    - 기본설정 사이드바의 제목 보다 하위가 좀 더 넓이가 작아보이도록 조절
    - google styled map을 참고해서 가독성이 좀 더 좋게 바꾸었습니다

--- src/components/Sidebar/SidebarContentFewer/SidebarContentThemePresenter.tsx
+++ src/components/Sidebar/SidebarContentFewer/SidebarContentThemePresenter.tsx
@@ -4,7 +4,7 @@ import styled from '../../../utils/styles/styled';
 import data from '../../../utils/redering-data/sidebarThemeData';
 
 const ThemeWrapper = styled.div`
-  padding: 50px 16px;
+  padding: 50px 8px;
 `;
 
 const Title = styled.h2`
@@ -13,6 +13,8 @@ const Title = styled.h2`
 `;
 
 const List = styled.ul`
+  width: 90%;
+  margin: 0 auto;
   margin-top: 25px;
 `;
 
