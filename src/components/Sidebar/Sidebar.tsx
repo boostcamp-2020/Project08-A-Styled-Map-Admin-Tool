b74ee39f0d71460fd5ca25afd331cbe615964861
eunsol0410 <eunsollee04@gmail.com>
2020-11-24 13:41:42 +0900

    [refactor] Hook 이름 변경
    - Hook 역할이 나타나고, 범용적으로 사용되도록 수정함

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '../../utils/styles/styled';
-import useSidebar, { useSidebarType } from '../../hooks/useSidebar';
+import useSidebar, { ToggleStatusHook } from '../../hooks/useToggleStatus';
 
 import SidebarHeader from './SidebarHeader/SidebarHeader';
 import SidebarContentFewer from './SidebarContentFewer/SidebarContent';
@@ -18,13 +18,13 @@ const SidebarWrapper = styled.div`
 `;
 
 function SidebarPresenter(): React.ReactElement {
-  const { isAdvanced, setIsAdvanced }: useSidebarType = useSidebar();
+  const { isActive, setIsActive }: ToggleStatusHook = useSidebar();
 
   return (
     <SidebarWrapper>
-      <SidebarHeader isAdvanced={isAdvanced} />
-      {isAdvanced ? <SidebarContentMore /> : <SidebarContentFewer />}
-      <SidebarFooter isAdvanced={isAdvanced} setIsAdvanced={setIsAdvanced} />
+      <SidebarHeader isAdvanced={isActive} />
+      {isActive ? <SidebarContentMore /> : <SidebarContentFewer />}
+      <SidebarFooter isAdvanced={isActive} setIsAdvanced={setIsActive} />
     </SidebarWrapper>
   );
 }
