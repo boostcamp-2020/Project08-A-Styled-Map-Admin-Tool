e0a0a4a56b4f121183e7feb70f64a00b03706da7
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-11-26 17:33:59 +0900

    [feat] combine reducer에 행정구역 reducer  추가
    - combine reducer에 행정구역 reducer 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from 'redux';
 import map from './map/reducer';
 import poi from './style/poiReducer';
+import administrative from './style/administrativeReducer';
 
-const rootReducer = combineReducers({ map, poi });
+const rootReducer = combineReducers({ map, poi, administrative });
 const store = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof rootReducer>;
