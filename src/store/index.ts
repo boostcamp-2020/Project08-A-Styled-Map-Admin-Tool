49ebc1a91445abfe0b60cc4a4ce8516ba0ca6136
gitgitwi <johnwi@knou.ac.kr>
2020-11-26 17:31:37 +0900

    [FEAT] [#31] 경관 메뉴 및 세부 메뉴 클릭에 따른 reducer 추가
    - `landscapeReducert.ts`에서 landscape 관련 스타일 reducer 정의
    - `featureTypeData.ts`에서 경관 메뉴 활성화 및 type 등록
    - `index.ts`에서 reducer 등록

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from 'redux';
 import map from './map/reducer';
 import poi from './style/poiReducer';
+import landscape from './style/landscapeReducer';
 
-const rootReducer = combineReducers({ map, poi });
+const rootReducer = combineReducers({ map, poi, landscape });
 const store = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof rootReducer>;
