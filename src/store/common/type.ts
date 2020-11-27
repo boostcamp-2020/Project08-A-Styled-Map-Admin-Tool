0481c428f664897ae566fe9fa338b473f6d736c1
eunsol0410 <eunsollee04@gmail.com>
2020-11-27 22:24:19 +0900

    [refactor] 공통 타입 분류, 상태 구조 변경
    - 기능유형/세부유형/스타일 관련 상태는 계속해서 사용하므로 store/common/type 으로 이동하였음
    - 기존 상태 구조는 세부 유형의 타입에 따라서 예외처리가 빈번하게 발생해서 불필요한 복잡도가 증가하였음. 따라서 Label이 text, icon으로 한 depth 더 들어가지 않고 동일하게 처리될 수 있도록 속성 수정함
    - 이를 반영해서 초기 속성 불러오는 함수도 수정하고, 함수명도 조금 더 직관적이도록 수정하였음

--- src/store/common/type.ts
+++ src/store/common/type.ts
@@ -1,9 +1,40 @@
-import { LabelType, CommonType } from './properties';
+export type ElementNameType = 'section' | 'labelText' | 'labelIcon';
+export type SubElementNameType = 'fill' | 'stroke';
 
-export interface FeatureState {
-  [name: string]: {
-    isChanged: boolean;
-    section: CommonType;
-    label: LabelType;
-  };
+export type FeatureNameOneType = 'water' | 'marker';
+export type FeatureNameMultiType =
+  | 'poi'
+  | 'administrative'
+  | 'landscape'
+  | 'road'
+  | 'transit';
+export type FeatureNameType = FeatureNameMultiType | FeatureNameOneType;
+
+export interface StyleType {
+  isChanged: boolean;
+  visibility: string;
+  color: string;
+  weight: number;
+  saturation: number;
+  lightness: number;
 }
+export interface ElementType {
+  fill: StyleType;
+  stroke: StyleType;
+}
+export interface FeatureType {
+  isChanged: boolean;
+  section: ElementType;
+  labelText: ElementType;
+  labelIcon: StyleType;
+}
+export interface FeatureState {
+  [name: string]: FeatureType;
+}
+
+export type ActionPayload = {
+  feature: string;
+  element: ElementNameType;
+  subElement?: SubElementNameType;
+  style: StyleType;
+};
