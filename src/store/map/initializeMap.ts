02e57862e3f91d756448e998c96657f2a2b26f42
박은식 <qkrdmstlr3@naver.com>
2020-11-28 17:35:00 +0900

    [feat] 지도의 라벨들 한국어로 번역
    - 지도의 라벨들을 한국어로 번역하였다

--- src/store/map/initializeMap.ts
+++ src/store/map/initializeMap.ts
@@ -6,12 +6,32 @@ import dotenv from 'dotenv';
 const LNG = 128;
 const LAT = 36.5;
 const ZOOM = 7;
+const LABELS = [
+  'country-label',
+  'settlement-label',
+  'road-label',
+  'state-label',
+  'settlement-subdivision-label',
+  'airport-label',
+  'transit-label',
+  'water-point-label',
+  'water-line-label',
+  'waterway-label',
+  'natural-point-label',
+  'natural-line-label',
+];
 
 dotenv.config();
 mapboxgl.accessToken = process.env.REACT_APP_ACCESS_TOKEN as string;
 
 interface InitializeMapProps {
   mapRef: RefObject<HTMLDivElement>;
+}
+
+function translate(map: mapboxgl.Map) {
+  LABELS.forEach((label) => {
+    map.setLayoutProperty(label, 'text-field', ['get', 'name_ko']);
+  });
 }
 
 function initializeMap({ mapRef }: InitializeMapProps): mapboxgl.Map {
@@ -31,6 +51,10 @@ function initializeMap({ mapRef }: InitializeMapProps): mapboxgl.Map {
     }),
     'bottom-right'
   );
+
+  map.on('load', () => {
+    translate(map);
+  });
 
   return map;
 }
