f983e7d7902cd10c7acec2889b47660df6c082b8
박은식 <qkrdmstlr3@naver.com>
2020-11-30 18:52:28 +0900

    [feat] 지도에 스타일을 적용할 수 있는 모듈을 만듦
    - 지도에 각각의 스타일들을 적용할 수 있게 여러 함수들을 모듈로 만듦
    - 각각의 타입(symbol line polygon)마다 서로 다른 값들을 인자로 주어야 함

---
+++ src/utils/applyStyle.ts
@@ -0,0 +1,62 @@
+import mapboxgl from 'mapbox-gl';
+import { hexToHSL } from './colorFormat';
+
+type colorType = 'fill-color' | 'line-color' | 'text-color' | 'text-halo-color';
+type weightType = 'line-width' | 'text-halo-width' | 'text-size';
+
+export function applyVisibility(
+  map: mapboxgl.Map,
+  layerName: string,
+  visibility: string
+): void {
+  map.setLayoutProperty(layerName, 'visibility', visibility);
+}
+
+export function applyColor(
+  map: mapboxgl.Map,
+  color: string,
+  type: colorType,
+  layerName: string
+): void {
+  const { h, s, l } = hexToHSL(color);
+  map.setPaintProperty(layerName, type, `hsl(${h}, ${s}, ${l})`);
+}
+
+export function applyWeight(
+  map: mapboxgl.Map,
+  weight: number,
+  layerName: string,
+  type: weightType
+): void {
+  map.setPaintProperty(layerName, type, weight * 2 + 1);
+}
+
+export function applySaturation(
+  map: mapboxgl.Map,
+  color: string,
+  saturation: number,
+  type: colorType,
+  layerName: string
+): void {
+  const { h, l } = hexToHSL(color);
+  map.setPaintProperty(
+    layerName,
+    type,
+    `hsl(${h}, ${50 + saturation / 2}, ${l})`
+  );
+}
+
+export function applyLightness(
+  map: mapboxgl.Map,
+  color: string,
+  layerName: string,
+  type: colorType,
+  lightness: number
+): void {
+  const { h, s } = hexToHSL(color);
+  map.setPaintProperty(
+    layerName,
+    type,
+    `hsl(${h}, ${s}, ${50 + lightness / 2})`
+  );
+}
---
+++ src/utils/colorFormat.ts
@@ -0,0 +1,42 @@
+/** https://css-tricks.com/converting-color-spaces-in-javascript/ */
+interface HexToHSLType {
+  h: number;
+  s: number;
+  l: number;
+}
+
+export function hexToHSL(color: string): HexToHSLType {
+  let r = parseInt(color.slice(1, 3), 10);
+  let g = parseInt(color.slice(3, 5), 10);
+  let b = parseInt(color.slice(5), 10);
+
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const cmin = Math.min(r, g, b);
+  const cmax = Math.max(r, g, b);
+  const delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
+
+  if (delta === 0) h = 0;
+  else if (cmax === r) h = ((g - b) / delta) % 6;
+  else if (cmax === g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return {
+    h,
+    s,
+    l,
+  };
+}
