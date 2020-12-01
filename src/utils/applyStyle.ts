6eda66c27e6a4c22abc9003d085dafb604a3e6e5
eunsol0410 <eunsollee04@gmail.com>
2020-12-01 15:55:53 +0900

    [fix] 스타일 적용 함수 수정
    - 텍스트 사이즈 조정은 상의 후 제거
    - mapbox 속성에 따라서 set 함수 달라서 조정함
    - 색상/채도/명도 중복되는 요소 많아서 하나로 조정

--- src/utils/applyStyle.ts
+++ src/utils/applyStyle.ts
@@ -10,7 +10,6 @@ export enum ColorTypeName {
 
 export enum WeightTypeName {
   'line-width' = 'line-width',
-  'text-size' = 'text-size',
   'text-halo-width' = 'text-halo-width',
 }
 
@@ -18,59 +17,66 @@ type colorType = keyof typeof ColorTypeName;
 
 type weightType = keyof typeof WeightTypeName;
 
-export function applyVisibility(
-  map: mapboxgl.Map,
-  layerName: string,
-  visibility: string
-): void {
-  map.setLayoutProperty(layerName, 'visibility', visibility);
+interface ApplyColorProps {
+  map: mapboxgl.Map;
+  layerNames: string[];
+  color: string;
+  type: colorType;
+  saturation?: number;
+  lightness?: number;
 }
 
-export function applyColor(
+export function applyVisibility(
   map: mapboxgl.Map,
-  layerName: string,
-  type: colorType,
-  color: string
+  layerNames: string[],
+  visibility: string
 ): void {
+  layerNames.forEach((layerName) => {
+    map.setLayoutProperty(layerName, 'visibility', visibility);
+  });
+}
+
+export function applyColor({
+  map,
+  layerNames,
+  color,
+  type,
+  saturation,
+  lightness,
+}: ApplyColorProps): void {
   const { h, s, l } = hexToHSL(color);
-  map.setPaintProperty(layerName, type, `hsl(${h}, ${s}%, ${l}%)`);
+  if (saturation) {
+    layerNames.forEach((layerName) => {
+      map.setPaintProperty(
+        layerName,
+        type,
+        `hsl(${h}, ${50 + saturation / 2}%, ${l}%)`
+      );
+    });
+    return;
+  }
+  if (lightness) {
+    layerNames.forEach((layerName) => {
+      map.setPaintProperty(
+        layerName,
+        type,
+        `hsl(${h}, ${s}%, ${50 + lightness / 2}%)`
+      );
+    });
+    return;
+  }
+  layerNames.forEach((layerName) => {
+    map.setPaintProperty(layerName, type, `hsl(${h}, ${s}%, ${l}%)`);
+  });
 }
 
 export function applyWeight(
   map: mapboxgl.Map,
-  layerName: string,
+  layerNames: string[],
   type: weightType,
   weight: number
 ): void {
-  map.setPaintProperty(layerName, type, weight * 2 + 1);
-}
-
-export function applySaturation(
-  map: mapboxgl.Map,
-  layerName: string,
-  type: colorType,
-  color: string,
-  saturation: number
-): void {
-  const { h, l } = hexToHSL(color);
-  map.setPaintProperty(
-    layerName,
-    type,
-    `hsl(${h}, ${50 + saturation / 2}%, ${l}%)`
-  );
-}
-
-export function applyLightness(
-  map: mapboxgl.Map,
-  layerName: string,
-  type: colorType,
-  color: string,
-  lightness: number
-): void {
-  const { h, s } = hexToHSL(color);
-  map.setPaintProperty(
-    layerName,
-    type,
-    `hsl(${h}, ${s}%, ${50 + lightness / 2}%)`
-  );
+  layerNames.forEach((layerName) => {
+    map.setPaintProperty(layerName, type, weight * 2 + 1);
+  });
 }
