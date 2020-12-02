dcda167964ecba19e4ce7975676e9341d4860b22
tejava <goddrinktejava@gmail.com>
2020-12-03 01:36:43 +0900

    [feat] landscape 상세 구분
    이전 구현 방식은 polygon만이 들어오는 가정을 했습니다.
    기존 맵에서 line, label 등이 존재하는 것을 확인하여 수정하였습니다.
    #93 을 참고하여 작성, 일관성을 추구하고자 하였습니다.

--- src/utils/map-styling/landscape.ts
+++ src/utils/map-styling/landscape.ts
@@ -1,62 +1,228 @@
 import { stylingProps } from './index';
-import { applyColor, applyVisibility, ColorType } from '../applyStyle';
-import { StyleKeyType, StyleType } from '../../store/common/type';
+import {
+  applyColor,
+  applyWeight,
+  applyVisibility,
+  WeightType,
+  ColorType,
+  StyleTypes,
+} from '../applyStyle';
+import {
+  StyleKeyType,
+  ElementNameType,
+  StyleType,
+} from '../../store/common/type';
 
 const SECTION = 'section';
-const FILL = 'fill';
+const LABELTEXT = 'labelText';
+const LABELICON = 'labelIcon';
+
 const ALL = 'all';
 
-const layers = ['human-made', 'building', 'natural', 'landcover'];
+type LandscapeSubFeature =
+  | 'all'
+  | 'human-made'
+  | 'building'
+  | 'natural'
+  | 'landcover';
 
-interface applyStyleProps {
-  map: mapboxgl.Map;
-  subFeatureName: string;
-  key: StyleKeyType;
-  style: StyleType;
+const humanMadeLayers: string[] = ['landscape-human-made'];
+
+const buildingLayers: string[] = [
+  'landscape-building',
+  'building-outline',
+  'building',
+];
+
+const naturalLayers: string[] = ['landscape-natural'];
+
+const landcoverLayers: string[] = ['landscape-landcover'];
+
+interface SubDetailType {
+  fill: string[];
+  stroke: string[];
 }
 
-function applyLandscapeStyle({
-  map,
-  subFeatureName,
-  key,
-  style,
-}: applyStyleProps): void {
-  const layerNames = [`landscape-${subFeatureName}`];
-
-  switch (key) {
-    case 'color':
-      applyColor({
-        map,
-        layerNames,
-        type: ColorType.fill,
-        color: style.color,
-      });
-      break;
-    case 'visibility':
-      applyVisibility({ map, layerNames, visibility: style.visibility });
-      break;
-    case 'lightness':
-      applyColor({
-        map,
-        layerNames,
-        type: ColorType.fill,
-        color: style.color,
-        lightness: Number(style.lightness),
-      });
-      break;
-    case 'saturation':
-      applyColor({
-        map,
-        layerNames,
-        type: ColorType.fill,
-        color: style.color,
-        saturation: Number(style.saturation),
-      });
-      break;
-    default:
-      break;
-  }
+interface DetailType {
+  section: SubDetailType;
+  labelText: SubDetailType;
+  labelIcon: SubDetailType;
 }
+
+function makeSubDetail(
+  fillLayers: string[],
+  strokeLayers: string[]
+): SubDetailType {
+  return { fill: fillLayers, stroke: strokeLayers };
+}
+
+const layersByType: { [key in LandscapeSubFeature]: DetailType } = {
+  'human-made': {
+    [SECTION]: makeSubDetail([], []),
+    [LABELTEXT]: makeSubDetail([], []),
+    [LABELICON]: makeSubDetail([], []),
+  },
+  building: {
+    [SECTION]: makeSubDetail([], []),
+    [LABELTEXT]: makeSubDetail([], []),
+    [LABELICON]: makeSubDetail([], []),
+  },
+  natural: {
+    [SECTION]: makeSubDetail([], []),
+    [LABELTEXT]: makeSubDetail([], []),
+    [LABELICON]: makeSubDetail([], []),
+  },
+  landcover: {
+    [SECTION]: makeSubDetail([], []),
+    [LABELTEXT]: makeSubDetail([], []),
+    [LABELICON]: makeSubDetail([], []),
+  },
+  all: {
+    [SECTION]: makeSubDetail([], []),
+    [LABELTEXT]: makeSubDetail([], []),
+    [LABELICON]: makeSubDetail([], []),
+  },
+};
+
+const VISIBLE = 1;
+const INVISIBLE = 0;
+
+const mappingDetailToFunc = {
+  labelText: {
+    fill: {
+      color: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: null,
+        funcName: () => null,
+      },
+      visibility: {
+        typeName: 'what is my name?',
+        funcName: applyVisibility,
+      },
+      isChanged: {
+        typeName: null,
+        funcName: () => null,
+      },
+    },
+    stroke: {
+      color: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.textHalo,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: WeightType.textHalo,
+        funcName: applyWeight,
+      },
+      isChanged: {
+        typeName: null,
+        funcName: () => null,
+      },
+    },
+  },
+  labelIcon: {
+    color: {
+      typeName: null,
+      funcName: () => null,
+    },
+    saturation: {
+      typeName: null,
+      funcName: () => null,
+    },
+    lightness: {
+      typeName: null,
+      funcName: () => null,
+    },
+    weight: {
+      typeName: null,
+      funcName: () => null,
+    },
+    visibility: {
+      typeName: ColorType.icon,
+      funcName: applyWeight,
+    },
+    isChanged: {
+      typeName: null,
+      funcName: () => null,
+    },
+  },
+  section: {
+    fill: {
+      color: {
+        typeName: ColorType.fill,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.fill,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.fill,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: null,
+        funcName: () => null,
+      },
+      visibility: {
+        typeName: ColorType.fill,
+        funcName: applyVisibility,
+      },
+      isChanged: {
+        typeName: null,
+        funcName: () => null,
+      },
+    },
+    stroke: {
+      color: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.line,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: WeightType.line,
+        funcName: applyVisibility,
+      },
+      isChanged: {
+        typeName: null,
+        funcName: () => null,
+      },
+    },
+  },
+};
 
 function landscapeStyling({
   map,
@@ -66,17 +232,68 @@ function landscapeStyling({
   key,
   style,
 }: stylingProps): void {
-  if (detailName !== SECTION || subDetailName !== FILL) {
-    return;
+  let type = null;
+  let func = null;
+
+  console.log(subFeatureName, detailName, subDetailName, key);
+
+  if (
+    detailName === ElementNameType.section ||
+    detailName === ElementNameType.labelText
+  ) {
+    const { typeName, funcName } = mappingDetailToFunc[detailName][
+      subDetailName
+    ][key as StyleKeyType];
+    type = typeName;
+    func = funcName;
+  } else {
+    const { typeName, funcName } = mappingDetailToFunc[detailName][
+      key as StyleKeyType
+    ];
+    type = typeName;
+    func = funcName;
   }
 
-  if (subFeatureName === ALL) {
-    layers.forEach((item) =>
-      applyLandscapeStyle({ map, subFeatureName: item, key, style })
-    );
-  } else {
-    applyLandscapeStyle({ map, subFeatureName, key, style });
+  if (!type) {
+    return;
   }
+  if (
+    key === 'visibility' &&
+    (type === ColorType.icon || type === WeightType.textHalo)
+  ) {
+    func({
+      map,
+      layerNames:
+        layersByType[subFeatureName as LandscapeSubFeature][detailName][
+          subDetailName
+        ],
+      type,
+      weight: style.visibility === 'none' ? INVISIBLE : VISIBLE,
+    });
+    return;
+  }
+  if (key === 'visibility') {
+    console.log(func);
+    func({
+      map,
+      layerNames:
+        layersByType[subFeatureName as LandscapeSubFeature][detailName][
+          subDetailName
+        ],
+      visibility: style.visibility,
+    });
+  }
+
+  func({
+    map,
+    layerNames:
+      layersByType[subFeatureName as LandscapeSubFeature][detailName][
+        subDetailName
+      ],
+    type: type as StyleTypes,
+    color: style.color,
+    [key]: style[key as StyleKeyType],
+  });
 }
 
 export default landscapeStyling;
