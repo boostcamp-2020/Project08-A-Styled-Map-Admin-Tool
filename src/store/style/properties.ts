1971640993eb8ed204ef6517f224b4aac2f4f3d0
eunsol0410 <eunsollee04@gmail.com>
2020-12-03 00:14:05 +0900

    [fix] 타입 에러 발생 해결, 프로퍼티 수정
    - common/type에 선언된 타입으로 바꾸면서 속성명 바뀜
    - 29줄 as StylePropsType 으로 타입 단언하여 에러 발생 제거함

--- src/store/style/properties.ts
+++ src/store/style/properties.ts
@@ -1,46 +1,14 @@
-// import { StyleType, ElementType, FeatureType } from '../common/type';
-
-// const style: StyleType = {
-//   isChanged: false,
-//   visibility: 'inherit',
-//   color: '#55bf40',
-//   weight: 0,
-//   saturation: 0,
-//   lightness: 0,
-// };
-
-// export const getDefaultStyle = (): StyleType => {
-//   return JSON.parse(JSON.stringify(style));
-// };
-
-// const getDefaultElement = (): ElementType => {
-//   return {
-//     fill: getDefaultStyle(),
-//     stroke: getDefaultStyle(),
-//   };
-// };
-
-// export const getDefaultFeature = (): FeatureType => {
-//   return {
-//     isChanged: false,
-//     section: getDefaultElement(),
-//     labelText: getDefaultElement(),
-//     labelIcon: getDefaultStyle(),
-//   };
-// };
-
 import {
   StyleType,
   ElementNameType,
   FeatureType,
-  FeatureNameType,
-  HasPropertiesType,
   SubElementNameType,
-  PropertyType,
-  nice,
+  FeaturePropsType,
+  ElementPropsType,
+  StylePropsType,
 } from '../common/type';
 
-import defaultStyle from '../../utils/rendering-data/layersColor2';
+import defaultStyle from '../../utils/rendering-data/layersColor';
 
 const style: StyleType = {
   isChanged: false,
@@ -51,70 +19,65 @@ const style: StyleType = {
   lightness: 0,
 };
 
-interface getDefaultStyleProps<T> {
-  featureType: FeatureNameType;
-  subFeatureType: T;
-  elementType: ElementNameType;
-  subElementType: SubElementNameType;
-}
-
 export const getDefaultStyle = ({
-  featureType,
-  subFeatureType,
-  elementType,
-  subElementType,
-}: getDefaultStyleProps): StyleType => {
+  feature,
+  subFeature,
+  element,
+  subElement,
+}: ElementPropsType): StyleType => {
+  const defaultColor = subElement
+    ? (defaultStyle[feature][subFeature][element] as StylePropsType)[subElement]
+    : defaultStyle[feature][subFeature][element];
   return {
     ...JSON.parse(JSON.stringify(style)),
-    color: defaultStyle[featureType][subFeatureType],
-    // defaultStyle[featureType][subFeatureType][elementType][subElementType],
+    color: defaultColor,
   };
 };
 
 export const getDefaultFeature = ({
-  featureType,
-  subFeatureType,
-}: HasPropertiesType): FeatureType => {
+  feature,
+  subFeature,
+}: FeaturePropsType): FeatureType => {
   return {
     isChanged: false,
-    section: defaultStyle[featureType][subFeatureType][ElementNameType.section]
+    section: defaultStyle[feature][subFeature][ElementNameType.section]
       ? {
           [SubElementNameType.fill]: getDefaultStyle({
-            featureType,
-            subFeatureType,
-            elementType: ElementNameType.section,
-            subElementType: SubElementNameType.fill,
+            feature,
+            subFeature,
+            element: ElementNameType.section,
+            subElement: SubElementNameType.fill,
           }),
           [SubElementNameType.stroke]: getDefaultStyle({
-            featureType,
-            subFeatureType,
-            elementType: ElementNameType.section,
-            subElementType: SubElementNameType.stroke,
+            feature,
+            subFeature,
+            element: ElementNameType.section,
+            subElement: SubElementNameType.stroke,
           }),
         }
       : null,
-    labelText: defaultStyle[featureType][subFeatureType][
-      ElementNameType.labelText
-    ]
+    labelText: defaultStyle[feature][subFeature][ElementNameType.labelText]
       ? {
           [SubElementNameType.fill]: getDefaultStyle({
-            featureType,
-            subFeatureType,
-            elementType: ElementNameType.labelText,
-            subElementType: SubElementNameType.fill,
+            feature,
+            subFeature,
+            element: ElementNameType.labelText,
+            subElement: SubElementNameType.fill,
           }),
           [SubElementNameType.stroke]: getDefaultStyle({
-            featureType,
-            subFeatureType,
-            elementType: ElementNameType.labelText,
-            subElementType: SubElementNameType.stroke,
+            feature,
+            subFeature,
+            element: ElementNameType.labelText,
+            subElement: SubElementNameType.stroke,
           }),
         }
       : null,
-    labelIcon: defaultStyle[featureType][subFeatureType][
-      ElementNameType.labelIcon
-    ]
-      ? getDefaultStyle()
+    labelIcon: defaultStyle[feature][subFeature][ElementNameType.labelIcon]
+      ? getDefaultStyle({
+          feature,
+          subFeature,
+          element: ElementNameType.labelIcon,
+        })
       : null,
   };
 };
