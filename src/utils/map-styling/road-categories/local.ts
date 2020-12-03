2421955f9717d4857e174f328980765ed6c86940
eunsol0410 <eunsollee04@gmail.com>
2020-12-03 13:16:24 +0900

    [feat] 도로 All 스타일 적용 추가

--- src/utils/map-styling/road-categories/local.ts
+++ src/utils/map-styling/road-categories/local.ts
@@ -1,5 +1,13 @@
 export const localLayerNames = {
-  all: ['ferry', 'ferry-auto', 'road-local', 'road-local-label'],
+  all: [
+    'road-local-polygon',
+    'ferry',
+    'ferry-auto',
+    'road-local',
+    'road-street',
+    'road-street-case',
+    'road-local-label',
+  ],
   polygon: ['road-local-polygon'],
   line: ['ferry', 'ferry-auto', 'road-local', 'road-street'],
   stroke: ['road-street-case'],
--- src/utils/map-styling/road.ts
+++ src/utils/map-styling/road.ts
@@ -2,6 +2,7 @@ import { stylingProps } from '.';
 import { arterialLayerNames } from './road-categories/arterial';
 import { localLayerNames } from './road-categories/local';
 import { sidewalkLayerNames } from './road-categories/sidewalk';
+import { allRoadLayerNames } from './road-categories/all';
 import stylingCategory from './road-categories/stylingCategory';
 
 function roadStyling({
@@ -12,9 +13,10 @@ function roadStyling({
   key,
   style,
 }: stylingProps): void {
-  type RoadSubFeatureType = 'arterial' | 'local' | 'sidewalk';
+  type RoadSubFeatureType = 'all' | 'arterial' | 'local' | 'sidewalk';
 
   const mappingSubFeatureLayerNames = {
+    all: allRoadLayerNames,
     arterial: arterialLayerNames,
     local: localLayerNames,
     sidewalk: sidewalkLayerNames,
