8a384d35f09c7ba0c6a2855119ebafb5a567233c
yyjjjj <lee.yeonjeong.lee@gmail.com>
2020-12-06 21:23:29 +0900

    [feat] ADD_LOG action에 대한 reducer 추가
    - ADD_LOG action에 대한 reducer 추가
    - 현재 local storage와 redux를 동기화하는 방식은 스타일설정마다 local
    storage에서 로그를 가져오고 새항목을 push하여 다시 설정하는 방식

--- src/store/history/reducer.ts
+++ src/store/history/reducer.ts
@@ -1,5 +1,26 @@
-import { INIT_HISTORY, TOGGLE_HISTORY } from './action';
-import { HistoryPropsType, HistoryActionType } from '../common/type';
+import { ADD_LOG, INIT_HISTORY, TOGGLE_HISTORY } from './action';
+import {
+  HistoryPropsType,
+  HistoryActionType,
+  HistoryInfoPropsType,
+  FeatureNameType,
+  ElementNameType,
+  SubElementNameType,
+  StyleType,
+} from '../common/type';
+import randomId from '../../utils/randomId';
+
+interface localStorageProps {
+  id: string;
+  value: string;
+  display: string;
+  changedKey: string | null;
+  feature: FeatureNameType;
+  subFeature: string | null;
+  element: ElementNameType | null;
+  subElement: SubElementNameType | null;
+  style: StyleType;
+}
 
 function historyReducer(
   state: HistoryPropsType,
@@ -7,17 +28,71 @@ function historyReducer(
 ): HistoryPropsType {
   switch (action.type) {
     case INIT_HISTORY: {
+      const log = localStorage.getItem('log')
+        ? JSON.parse(localStorage.getItem('log') as string)
+        : [];
+
       return {
+        ...state,
         isHistoryOpen: false,
+        log: log.map((item: localStorageProps) => {
+          return { id: item.id, display: item.display };
+        }),
       };
     }
     case TOGGLE_HISTORY: {
       return {
+        ...state,
         isHistoryOpen: !state.isHistoryOpen,
       };
     }
+    case ADD_LOG: {
+      const {
+        value,
+        changedKey,
+        feature,
+        subFeature,
+        element,
+        subElement,
+        style,
+      } = action.payload as HistoryInfoPropsType;
+
+      if (state.log !== undefined) {
+        const id = randomId(8);
+        state.log.push({
+          id,
+          display: `${feature} -> ${subFeature} -> ${element} -> ${subElement}에서 ${changedKey} 항목에 대하여\n ${value}로의 변화가 있었습니다.`,
+        });
+        const storedLog =
+          localStorage.getItem('log') === null
+            ? []
+            : JSON.parse(localStorage.getItem('log') as string);
+
+        if (storedLog !== undefined) {
+          storedLog.push({
+            id,
+            value,
+            display: `${feature} -> ${subFeature} -> ${element} -> ${subElement}에서 ${changedKey} 항목에 대하여\n ${value}로의 변화가 있었습니다.`,
+            changedKey,
+            feature,
+            subFeature,
+            element,
+            subElement,
+            style,
+          });
+          localStorage.setItem('log', JSON.stringify(storedLog));
+        }
+      }
+      return {
+        ...state,
+      };
+    }
+
     default:
-      return { isHistoryOpen: false };
+      return {
+        ...state,
+        isHistoryOpen: false,
+      };
   }
 }
 
