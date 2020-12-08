7eb28c8d488be6c52a44063443b5aad4174eccdc
박은식 <qkrdmstlr3@naver.com>
2020-12-08 17:28:22 +0900

    [feat] 로그 클릭시 그 때의 상태와 비교하는 기능 추가
    - 로그를 클릭하면 그 상태와 현재 상태가 반으로 갈라져서 보여진다
    - setFeatureStyle를 이용해서 지도 로딩 후 스타일을 변경시켜주었다

--- src/hooks/map/useCompareFeature.ts
+++ src/hooks/map/useCompareFeature.ts
@@ -1,23 +1,25 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
 import { useState, RefObject, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import getCompareMap from './getCompareMap';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
-import 'mapbox-gl-compare/style.css';
+import {
+  HistoryPropsType,
+  FeatureNameType,
+  FeatureState,
+} from '../../store/common/type';
+import setFeatureStyle from '../../utils/setFeatureStyle';
 
 export interface mapProps {
   container: string | HTMLElement;
   [key: string]: string | HTMLElement;
 }
 
-export type comparisonButtonClickHandlerType = (
-  afterMapProps?: mapProps,
-  beforeMapProps?: mapProps
-) => void;
-
 export interface useComparisonButtonType {
-  isCompareActive: boolean;
-  comparisonButtonClickHandler: comparisonButtonClickHandlerType;
+  logId: string | undefined;
+  comparisonButtonClickHandler: (id: string) => void;
 }
 
 export interface useCompareFeatureProps {
@@ -29,11 +31,14 @@ function useCompareFeature({
   containerRef,
   beforeMapRef,
 }: useCompareFeatureProps): useComparisonButtonType {
-  const [isCompareActive, setIsCompareActive] = useState(false);
+  const [logId, setLogId] = useState<string>();
   const map = useSelector<RootState>((state) => state.map.map) as mapboxgl.Map;
+  const history = useSelector<RootState>(
+    (state) => state.history
+  ) as HistoryPropsType;
 
   useEffect(() => {
-    if (!isCompareActive) return;
+    if (!logId) return;
 
     const beforeMap = new mapboxgl.Map({
       container: beforeMapRef.current as HTMLDivElement,
@@ -42,23 +47,42 @@ function useCompareFeature({
       zoom: map.getZoom(),
     });
 
+    beforeMap.on('load', () => {
+      if (!history) return;
+      const [log] = history.log?.filter((log) => log.id === logId) || [];
+
+      if (!beforeMap) {
+        return;
+      }
+
+      for (const feature in log.wholeStyle) {
+        setFeatureStyle({
+          map: beforeMap as mapboxgl.Map,
+          feature: feature as FeatureNameType,
+          featureState: log.wholeStyle[
+            feature as FeatureNameType
+          ] as FeatureState,
+        });
+      }
+    });
     const compare = getCompareMap(beforeMap, map, containerRef.current);
 
     // eslint-disable-next-line consistent-return
     return () => {
       compare.remove();
     };
-  }, [isCompareActive]);
+  }, [logId]);
 
-  const comparisonButtonClickHandler = (
-    afterMapProps?: mapProps,
-    beforeMapProps?: mapProps
-  ) => {
-    setIsCompareActive(!isCompareActive);
+  const comparisonButtonClickHandler = (newLogId: string) => {
+    if (newLogId === logId) {
+      setLogId(undefined);
+      return;
+    }
+    setLogId(newLogId);
   };
 
   return {
-    isCompareActive,
+    logId,
     comparisonButtonClickHandler,
   };
 }
