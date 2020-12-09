cb1072cf067596ca1a8be662b6f0baa632842444
박은식 <qkrdmstlr3@naver.com>
2020-12-10 01:26:39 +0900

    [refactor] init파일에서 주요도로의 일부분을 중심도로로 편입함
    - 주요도로의 일부분을 네이밍.색 변경해서 중심도로로 편입₩

---
+++ src/utils/map-styling/road-categories/highway.ts
@@ -0,0 +1,28 @@
+export const highwayLayerNames = {
+  all: [
+    'road-highway-section-fill-1',
+    'road-highway-section-fill-2',
+    // 'road-highway-section-fill-3',
+    'road-highway-section-fill-4',
+    'road-highway-section-fill-5',
+    'road-highway-section-fill-6',
+    'road-highway-section-fill-7',
+  ],
+  polygon: [],
+  line: [
+    'road-highway-section-fill-1',
+    'road-highway-section-fill-2',
+    // 'road-highway-section-fill-3',
+    'road-highway-section-fill-4',
+    'road-highway-section-fill-5',
+    'road-highway-section-fill-6',
+    'road-highway-section-fill-7',
+  ],
+  stroke: ['road-highway-section-stroke-1', 'road-highway-section-stroke-2'],
+  text: {
+    all: [],
+    hasStroke: [],
+    noStroke: [],
+  },
+  icon: [],
+};
