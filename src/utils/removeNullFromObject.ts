4211c14509e62dc6d20bfd1e3ae6c2dac805baf6
박은식 <qkrdmstlr3@naver.com>
2020-12-10 21:32:04 +0900

    [feat] object에서 null을 제거하는 모듈 작성
    - 로그에 들어가는 wholeStyle에는 value가 null인 값이 있으면 안된다.
    - 따라서 넣기 전에 스타일을 한번 거르는 모듈을 작성함

---
+++ src/utils/removeNullFromObject.ts
@@ -0,0 +1,19 @@
+import { objType } from '../store/common/type';
+
+function removeNullFromObject(object: objType): any {
+  if (typeof object !== 'object') return;
+
+  Object.keys(object).forEach((key) => {
+    if (object[key] === null) {
+      // eslint-disable-next-line no-param-reassign
+      delete object[key];
+      return;
+    }
+    removeNullFromObject(object[key]);
+  });
+
+  // eslint-disable-next-line consistent-return
+  return object;
+}
+
+export default removeNullFromObject;
