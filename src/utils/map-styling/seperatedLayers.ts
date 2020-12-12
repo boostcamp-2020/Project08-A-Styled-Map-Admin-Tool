bd72f719b12d7c2c3dbacd0f1d316c631932c56c
박은식 <qkrdmstlr3@naver.com>
2020-12-12 13:58:30 +0900

    [feat] init의 레이어들을 구별할 수 있는 seperatedLayers파일 추가
    - init의 레이어들을 구별 가능하도록 seperatedLayers파일을 추가함

---
+++ src/utils/map-styling/seperatedLayers.ts
@@ -0,0 +1,52 @@
+import initLayers from '../rendering-data/layers/init';
+
+const seperatedLayers: any = {};
+
+function isNumber(value: string | number): boolean {
+  return !Number.isNaN(Number(value));
+}
+
+/** feature / subFeature / element / subElement / value */
+initLayers.layers.forEach((layer) => {
+  const layerId = layer.id;
+  const layerInfo = { id: layerId, type: layer.type };
+  const [feature, subFeature, element, subElement] = layerInfo.id.split('-');
+  if (feature && !seperatedLayers[feature]) {
+    seperatedLayers[feature] = { all: {} };
+  }
+  if (subFeature && !seperatedLayers[feature][subFeature]) {
+    seperatedLayers[feature][subFeature] = {};
+  }
+  if (element && seperatedLayers[feature][subFeature][element] === undefined) {
+    if (subElement && !isNumber(subElement) && subElement !== undefined) {
+      seperatedLayers[feature][subFeature][element] = {};
+      seperatedLayers[feature].all[element] = {};
+    } else {
+      seperatedLayers[feature][subFeature][element] = [];
+      seperatedLayers[feature].all[element] = [].concat(
+        seperatedLayers[feature].all[element] || []
+      );
+    }
+  }
+  if (
+    !isNumber(subElement) &&
+    subElement !== undefined &&
+    seperatedLayers[feature][subFeature][element][subElement] === undefined
+  ) {
+    seperatedLayers[feature][subFeature][element][subElement] = [];
+    seperatedLayers[feature].all[element][subElement] = [].concat(
+      seperatedLayers[feature].all[element][subElement] || []
+    );
+  }
+  if (!isNumber(subElement) && subElement !== undefined) {
+    seperatedLayers[feature][subFeature][element][subElement].push(layerInfo);
+    seperatedLayers[feature].all[element][subElement].push(layerInfo);
+  } else {
+    seperatedLayers[feature][subFeature][element].push(layerInfo);
+    seperatedLayers[feature].all[element].push(layerInfo);
+  }
+});
+
+console.log(seperatedLayers);
+
+export default seperatedLayers;
