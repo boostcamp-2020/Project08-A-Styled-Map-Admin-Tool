e7c31fd06cb42446d3b38f3db41364c7588fcc31
박은식 <qkrdmstlr3@naver.com>
2020-12-13 13:21:14 +0900

    [feat] 각 스타일링 별로 mapping 변수를 한군데에 모아서 중복을 제거할수 있도록 파일 만듦
    - 여섯 스타일링 파일에있는 여섯개의 mapping변수를 한군데에 모음
    - 중복을 제거할 수 있고 한번에 관리가 가능해짐

--- src/utils/map-styling/macgyver/mappingDetailToFunc.ts
+++ src/utils/map-styling/macgyver/mappingDetailToFunc.ts
@@ -7,6 +7,7 @@ import {
   VisibilityType,
 } from '../../applyStyle';
 
+/** template */
 const labelTextTemplate = {
   fill: {
     color: {
@@ -54,6 +55,78 @@ const labelTextTemplate = {
   },
 };
 
+const sectionFillTemplate = {
+  color: {
+    typeName: ColorType.fill,
+    funcName: applyColor,
+  },
+  saturation: {
+    typeName: ColorType.fill,
+    funcName: applyColor,
+  },
+  lightness: {
+    typeName: ColorType.fill,
+    funcName: applyColor,
+  },
+  weight: {
+    typeName: null,
+    funcName: null,
+  },
+  visibility: {
+    typeName: VisibilityType.visible,
+    funcName: applyVisibility,
+  },
+};
+
+const sectionStrokeTemplate = {
+  color: {
+    typeName: ColorType.line,
+    funcName: applyColor,
+  },
+  saturation: {
+    typeName: ColorType.line,
+    funcName: applyColor,
+  },
+  lightness: {
+    typeName: ColorType.line,
+    funcName: applyColor,
+  },
+  weight: {
+    typeName: WeightType.line,
+    funcName: applyWeight,
+  },
+  visibility: {
+    typeName: VisibilityType.visible,
+    funcName: applyVisibility,
+  },
+};
+
+const nullTemplate = {
+  color: {
+    typeName: null,
+    funcName: null,
+  },
+  saturation: {
+    typeName: null,
+    funcName: null,
+  },
+  lightness: {
+    typeName: null,
+    funcName: null,
+  },
+  weight: {
+    typeName: null,
+    funcName: null,
+  },
+  visibility: {
+    typeName: null,
+    funcName: null,
+  },
+};
+
+/**
+ * mapping variable
+ */
 export const poiMappingToFunc = {
   labelText: labelTextTemplate,
   labelIcon: {
@@ -62,4 +135,71 @@ export const poiMappingToFunc = {
       funcName: applyWeight,
     },
   },
+};
+
+export const administrativeMappingToFunc = {
+  section: {
+    fill: nullTemplate,
+    stroke: sectionStrokeTemplate,
+  },
+  labelText: labelTextTemplate,
+};
+
+export const landscapeMappingToFunc = {
+  section: {
+    fill: sectionFillTemplate,
+    stroke: sectionStrokeTemplate,
+  },
+  labelText: labelTextTemplate,
+};
+
+export const roadMappingToFunc = {
+  section: {
+    fill: {
+      color: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.line,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: VisibilityType.visible,
+        funcName: applyVisibility,
+      },
+    },
+    stroke: sectionStrokeTemplate,
+  },
+  labelText: labelTextTemplate,
+  labelIcon: {
+    visibility: {
+      typeName: ColorType.icon,
+      funcName: applyVisibility,
+    },
+  },
+};
+
+export const transitMappingToFunc = {
+  section: {
+    fill: sectionFillTemplate,
+    stroke: sectionStrokeTemplate,
+  },
+  labelText: labelTextTemplate,
+};
+
+export const waterMappingToFunc = {
+  section: {
+    fill: sectionFillTemplate,
+    stroke: nullTemplate,
+  },
+  labelText: labelTextTemplate,
 };
