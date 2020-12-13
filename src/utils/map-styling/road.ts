0806614522756c8be203952491512499660ef98d
박은식 <qkrdmstlr3@naver.com>
2020-12-13 12:03:46 +0900

    [refactor] road 스타일 파일 리팩토링
    - poi를 참고해서 mapping하는 변수를 만들고 그 변수에서 함수와 타입을 꺼내서 사용
    - if문의 depth를 1단계로 줄임

--- src/utils/map-styling/road.ts
+++ src/utils/map-styling/road.ts
@@ -1,10 +1,121 @@
 import { stylingProps } from '.';
-import { highwayLayerNames } from './road-categories/highway';
-import { arterialLayerNames } from './road-categories/arterial';
-import { localLayerNames } from './road-categories/local';
-import { sidewalkLayerNames } from './road-categories/sidewalk';
-import { allRoadLayerNames } from './road-categories/all';
-import stylingCategory from './road-categories/stylingCategory';
+import {
+  applyColor,
+  applyWeight,
+  applyVisibility,
+  WeightType,
+  ColorType,
+  VisibilityType,
+} from '../applyStyle';
+import { StyleKeyType, ElementNameType } from '../../store/common/type';
+import seperatedLayers from './macgyver/seperatedLayers';
+import {
+  getIdsFromLayersArr,
+  getIdsFromLayersArrWithType,
+  getIdsFromLayersArrWithoutType,
+} from './macgyver/utils';
+import weightTemplate from './macgyver/weightTemplate';
+
+const mappingDetailToFunc = {
+  section: {
+    fill: {
+      color: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.line,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: VisibilityType.visible,
+        funcName: applyVisibility,
+      },
+    },
+    stroke: {
+      color: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.line,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.line,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: VisibilityType.visible,
+        funcName: applyVisibility,
+      },
+    },
+  },
+  labelText: {
+    fill: {
+      color: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.text,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: null,
+        funcName: null,
+      },
+      visibility: {
+        typeName: VisibilityType.visible,
+        funcName: applyVisibility,
+      },
+    },
+    stroke: {
+      color: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      saturation: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      lightness: {
+        typeName: ColorType.textHalo,
+        funcName: applyColor,
+      },
+      weight: {
+        typeName: WeightType.textHalo,
+        funcName: applyWeight,
+      },
+      visibility: {
+        typeName: WeightType.textHalo,
+        funcName: applyWeight,
+      },
+    },
+  },
+  labelIcon: {
+    visibility: {
+      typeName: ColorType.icon,
+      funcName: applyVisibility,
+    },
+  },
+};
 
 function roadStyling({
   map,
@@ -14,24 +125,67 @@ function roadStyling({
   key,
   style,
 }: stylingProps): void {
-  type RoadSubFeatureType = 'all' | 'arterial' | 'local' | 'sidewalk';
+  if (key === 'isChanged') return;
+  let type = null;
+  let func = null;
 
-  const mappingSubFeatureLayerNames = {
-    all: allRoadLayerNames,
-    highway: highwayLayerNames,
-    arterial: arterialLayerNames,
-    local: localLayerNames,
-    sidewalk: sidewalkLayerNames,
-  };
+  if (element === ElementNameType.labelIcon) {
+    if (key !== 'visibility') return;
+    const { typeName, funcName } = mappingDetailToFunc[element][key];
+    type = typeName;
+    func = funcName;
+  } else {
+    const { typeName, funcName } = mappingDetailToFunc[element][subElement][
+      key
+    ];
+    type = typeName;
+    func = funcName;
+  }
 
-  stylingCategory({
-    layerNames: mappingSubFeatureLayerNames[subFeature as RoadSubFeatureType],
+  /** get LayerNames */
+  if (!type || !func) return;
+  let layerNames: string[] = [];
+  let outsideLayerNames: string[] = [];
+  if (
+    element === ElementNameType.labelText ||
+    element === ElementNameType.labelIcon
+  ) {
+    layerNames = getIdsFromLayersArr(seperatedLayers.road[subFeature][element]);
+  } else {
+    layerNames = getIdsFromLayersArrWithoutType(
+      seperatedLayers.road[subFeature][element][subElement],
+      'fill'
+    );
+    outsideLayerNames = getIdsFromLayersArrWithType(
+      seperatedLayers.road[subFeature][element][subElement],
+      'fill'
+    );
+  }
+
+  /** set weight with weightTemplate */
+  let zoomWeight = null;
+  if (key === 'weight') {
+    zoomWeight = weightTemplate[subFeature][subElement](style.weight);
+  }
+
+  /** styling */
+  // fill일 때만 조건문 추가
+  if (outsideLayerNames.length && key !== 'weight') {
+    func({
+      map,
+      layerNames: outsideLayerNames,
+      type: ColorType.fill,
+      color: '#ffffff',
+      [key]: style[key as StyleKeyType],
+    });
+  }
+
+  func({
     map,
-    subFeature,
-    element,
-    subElement,
-    key,
-    style,
+    layerNames,
+    type,
+    color: style.color,
+    [key]: zoomWeight || style[key as StyleKeyType],
   });
 }
 
