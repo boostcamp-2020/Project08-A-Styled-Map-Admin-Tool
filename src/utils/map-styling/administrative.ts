5a66fc3eb3e864a384f0b078533938dc44ca9d24
박은식 <qkrdmstlr3@naver.com>
2020-12-13 13:22:19 +0900

    [refactor] administrative스타일링에서 mapping변수 제거
    - administrative스타일링에서 mapping변수를 제거하였다
    - macgyver/mapping함수에서 가져오도록 변경

--- src/utils/map-styling/administrative.ts
+++ src/utils/map-styling/administrative.ts
@@ -1,110 +1,9 @@
 import { stylingProps } from '.';
 import { ElementNameType, StyleKeyType } from '../../store/common/type';
-import {
-  ColorType,
-  WeightType,
-  VisibilityType,
-  applyVisibility,
-  applyColor,
-  applyWeight,
-} from '../applyStyle';
+import { WeightType } from '../applyStyle';
 import seperatedLayers from './macgyver/seperatedLayers';
 import { getIdsFromLayersArr, INVISIBLE, VISIBLE } from './macgyver/utils';
-
-const mappingDetailToFunc = {
-  section: {
-    fill: {
-      color: {
-        typeName: null,
-        funcName: null,
-      },
-      saturation: {
-        typeName: null,
-        funcName: null,
-      },
-      lightness: {
-        typeName: null,
-        funcName: null,
-      },
-      weight: {
-        typeName: null,
-        funcName: null,
-      },
-      visibility: {
-        typeName: null,
-        funcName: null,
-      },
-    },
-    stroke: {
-      color: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: WeightType.line,
-        funcName: applyWeight,
-      },
-      visibility: {
-        typeName: VisibilityType.visible,
-        funcName: applyVisibility,
-      },
-    },
-  },
-  labelText: {
-    fill: {
-      color: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: null,
-        funcName: null,
-      },
-      visibility: {
-        typeName: VisibilityType.visible,
-        funcName: applyVisibility,
-      },
-    },
-    stroke: {
-      color: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: WeightType.textHalo,
-        funcName: applyWeight,
-      },
-      visibility: {
-        typeName: WeightType.textHalo,
-        funcName: applyWeight,
-      },
-    },
-  },
-};
+import { administrativeMappingToFunc } from './macgyver/mappingDetailToFunc';
 
 function administrativeStyling({
   map,
@@ -116,9 +15,9 @@ function administrativeStyling({
 }: stylingProps): void {
   if (key === 'isChanged' || element === ElementNameType.labelIcon) return;
 
-  const { typeName: type, funcName: func } = mappingDetailToFunc[element][
-    subElement
-  ][key];
+  const { typeName: type, funcName: func } = administrativeMappingToFunc[
+    element
+  ][subElement][key];
   if (!type || !func) return;
 
   /** get LayerNames */
