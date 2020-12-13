129e0f77677fe42048c6de109372f2618ff85636
박은식 <qkrdmstlr3@naver.com>
2020-12-13 13:23:31 +0900

    [refactor] landscape 스타일링에서 mapping변수 제거
    - landscape스타일링에서 mapping변수를 제거하였다
    - macgyver/mapping함수에서 가져오도록 변경

--- src/utils/map-styling/landscape.ts
+++ src/utils/map-styling/landscape.ts
@@ -1,12 +1,5 @@
 import { stylingProps } from './index';
-import {
-  applyColor,
-  applyWeight,
-  applyVisibility,
-  WeightType,
-  ColorType,
-  VisibilityType,
-} from '../applyStyle';
+import { WeightType, ColorType } from '../applyStyle';
 import { StyleKeyType, ElementNameType } from '../../store/common/type';
 import seperatedLayers from './macgyver/seperatedLayers';
 import {
@@ -16,101 +9,7 @@ import {
   INVISIBLE,
   VISIBLE,
 } from './macgyver/utils';
-
-const mappingDetailToFunc = {
-  section: {
-    fill: {
-      color: {
-        typeName: ColorType.fill,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.fill,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.fill,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: null,
-        funcName: null,
-      },
-      visibility: {
-        typeName: VisibilityType.visible,
-        funcName: applyVisibility,
-      },
-    },
-    stroke: {
-      color: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.line,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: WeightType.line,
-        funcName: applyWeight,
-      },
-      visibility: {
-        typeName: VisibilityType.visible,
-        funcName: applyVisibility,
-      },
-    },
-  },
-  labelText: {
-    fill: {
-      color: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.text,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: null,
-        funcName: null,
-      },
-      visibility: {
-        typeName: VisibilityType.visible,
-        funcName: applyVisibility,
-      },
-    },
-    stroke: {
-      color: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      saturation: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      lightness: {
-        typeName: ColorType.textHalo,
-        funcName: applyColor,
-      },
-      weight: {
-        typeName: WeightType.textHalo,
-        funcName: applyWeight,
-      },
-      visibility: {
-        typeName: WeightType.textHalo,
-        funcName: applyWeight,
-      },
-    },
-  },
-};
+import { landscapeMappingToFunc } from './macgyver/mappingDetailToFunc';
 
 function landscapeStyling({
   map,
@@ -122,7 +21,7 @@ function landscapeStyling({
 }: stylingProps): void {
   if (key === 'isChanged' || element === ElementNameType.labelIcon) return;
 
-  const { typeName: type, funcName: func } = mappingDetailToFunc[element][
+  const { typeName: type, funcName: func } = landscapeMappingToFunc[element][
     subElement
   ][key];
   if (!type || !func) return;
