20b327a3287dc44b85d723c9645a87da8be72ac7
gitgitwi <johnwi@knou.ac.kr>
2020-12-14 13:47:54 +0900

    [FEAT] 스케치 테마 추가

--- src/utils/rendering-data/sidebarThemeData.ts
+++ src/utils/rendering-data/sidebarThemeData.ts
@@ -1,6 +1,7 @@
 import christmas from './theme/christmas.json';
 import blueprint from './theme/blueprint.json';
 import dark from './theme/dark.json';
+import sketch from './theme/sketch.json';
 
 const data = [
   {
@@ -21,7 +22,8 @@ const data = [
   {
     src:
       'https://i.pinimg.com/originals/8f/6d/97/8f6d971f3d086edaf6cee773991abb27.jpg',
-    name: '흑백',
+    name: '스케치',
+    theme: sketch,
   },
   {
     src:
