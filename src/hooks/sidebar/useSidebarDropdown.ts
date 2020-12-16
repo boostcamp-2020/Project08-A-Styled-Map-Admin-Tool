ae19cd6cac673d18f73a3f7de5ca654ef978154a
eunsol0410 <eunsollee04@gmail.com>
2020-12-16 23:53:44 +0900

    [feat] 사이드바 헤더 초기화 클릭시 테마, 표기단계도 같이 초기화

--- src/hooks/sidebar/useSidebarDropdown.ts
+++ src/hooks/sidebar/useSidebarDropdown.ts
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { ReplaceType } from '../../store/common/type';
+import { initDepthTheme } from '../../store/depth-theme/action';
 import useWholeStyle from '../common/useWholeStyle';
 
 interface SidebarDropdownProps {
@@ -17,6 +19,7 @@ function useSidebarDropdown({
   isOpened,
   dropdownToggleHandler,
 }: SidebarDropdownProps): useSidebarDropdownType {
+  const dispatch = useDispatch();
   const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
   const { changeStyle } = useWholeStyle();
 
@@ -27,6 +30,7 @@ function useSidebarDropdown({
 
   const resetClickHandler = () => {
     changeStyle({}, { changedKey: ReplaceType.init });
+    dispatch(initDepthTheme());
   };
 
   return {
